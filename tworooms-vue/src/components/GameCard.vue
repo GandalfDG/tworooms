796c13c11c1e6752f02b9eac0ec66b887a6e83ed
Jack Case <jackacase@gmail.com>
2022-06-22 20:02:56 +0000

    getting started with vue (again)

---
+++ tworooms-vue/src/components/GameCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {reactive} from 'vue'
+
+const card = reactive({
+    title: "president",
+    description: "don't get got",
+    color: "blue",
+    image: "https://fakeimg.pl/640x360"
+})
+</script>
+
+<template>
+    <div>
+        <h1>{{card.title}}</h1>
+        <img :src="card.image"/>
+        <h3>{{card.description}}</h3>
+    </div>   
+</template>
