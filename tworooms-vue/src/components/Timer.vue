7eeebece43a81736a194ca75ca0e16324fb7feec
Jack Case <jackacase@gmail.com>
2022-07-01 17:57:35 +0000

    improved timer reliability removed need for a tick reactive property which was previously used to trigger recompute

--- tworooms-vue/src/components/Timer.vue
+++ tworooms-vue/src/components/Timer.vue
@@ -1,24 +1,33 @@
 <script setup>
 import { reactive, computed, watch } from 'vue'
+import { START_LOCATION } from 'vue-router';
 
 const time = reactive({
     start_time: 0, //new Date().getTime(),
     duration: 60,
-    ticks: 0,
+    seconds_remaining: 0,
     paused: true
 })
 
+const update_rate_ms = 200
+
 const timestring = computed(() => {
     // duration - current time - start time
-    let current_time = 0 //new Date().getTime()
-    let seconds_remaining = time.duration - (time.ticks % time.duration)//current_time - time.start_time
+    if (time.start_time === 0) {
+        time.start_time = new Date().getTime()
+    }
     // format text nicely
-    let minutes = (Math.floor(seconds_remaining / 60)).toString()
-    let seconds = (seconds_remaining % 60).toString()
+    let minutes = (Math.floor(time.seconds_remaining / 60)).toString()
+    let seconds = (Math.floor(time.seconds_remaining % 60)).toString()
 
     let padded_seconds = seconds.length < 2 ? "0" + seconds : seconds
     return "".concat(minutes, ":", padded_seconds)
 })
+
+function update_time() {
+    let current_time = new Date().getTime()
+    time.seconds_remaining = time.duration - ((current_time - time.start_time) / 1000)
+}
 
 var timer_interval
 
@@ -26,13 +35,14 @@ var timer_interval
 watch(() => time.paused, () => {
     if (!time.paused) {
         // set interval to tick
-        timer_interval = setInterval(() => { time.ticks++ }, 1000)
+        timer_interval = setInterval(()=>{update_time()}, update_rate_ms)
     }
     else {
         // stop the interval
         clearInterval(timer_interval)
     }
 })
+
 
 </script>
 
