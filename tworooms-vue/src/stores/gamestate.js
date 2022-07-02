2cf83741ca9da27dcd67e3fa9331ff6862e2cc6f
Jack Case <jackacase@gmail.com>
2022-07-02 00:06:52 +0000

    added axios for http requests

--- tworooms-vue/src/stores/gamestate.js
+++ tworooms-vue/src/stores/gamestate.js
@@ -1,4 +1,10 @@
 import { defineStore } from 'pinia'
+import axios from 'axios'
+
+const ax = axios.create({
+    baseURL: 'localhost:1337/',
+    timeout: 1000,
+})
 
 export const useGameState = defineStore('gamestate', {
     state: () => (
@@ -7,5 +13,21 @@ export const useGameState = defineStore('gamestate', {
             ishost: false,
             roomcode: 'ABCD'
         }
-    )
+    ),
+
+    actions: {
+        async createRoom() {
+            let response = await ax.post('create/', {
+                playername: this.playername
+            })
+            this.roomcode = response.data.roomcode
+        },
+        
+        async joinRoom() {
+            let response = await ax.post('join/', {
+                playername: this.playername,
+                roomcode: this.roomcode
+            })
+        }
+    }
 })
