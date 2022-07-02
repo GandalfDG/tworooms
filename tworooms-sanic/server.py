39d6a4973a42b2eab10a7f649d3b5e6b8efe04a0
Jack Case <jackacase@gmail.com>
2022-07-02 19:42:18 +0000

    sanic now serves the static assets

--- tworooms-sanic/server.py
+++ tworooms-sanic/server.py
@@ -1,6 +1,8 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json, file
 from sanic_cors import CORS, cross_origin
+
+from os import path
 
 app = Sanic("tworooms")
 CORS(app)
@@ -9,10 +11,20 @@ CORS(app)
 # this will be replaced with redis for "production"
 app.ctx.gamedata = {}
 
+app.static("/assets", "/workspaces/tworooms/tworooms-vue/dist/assets")
 
-@app.post("create/")
+
+async def get_app(request, ext=None):
+    return await file(location="/workspaces/tworooms/tworooms-vue/dist/index.html")
+
+app.add_route(get_app, "/<ext>/")
+app.add_route(get_app, "/")
+
+
+@app.post("api/create/")
 async def create_room_handler(request):
     return json(
         {
             "roomcode": request.json['playername']
         })
+
