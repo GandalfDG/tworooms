c83a498093df6424d292e3d28081d54a88fab8d2
Jack Case <jackacase@gmail.com>
2022-08-03 19:53:16 -0400

    fixed playerlist handling in the lobby

--- frontend/src/gamelogic.js
+++ frontend/src/gamelogic.js
@@ -4,7 +4,12 @@ export const wsEvent = new Event('wsmessage');
 export function wsMessageListener(event) {
     // big-ish case statement here for different messages
     let gamestate = useGameState();
-    let msgdata = event.data;
+    let msgdata = JSON.parse(event.data);
+
+    if (msgdata['playerlist']) {
+        gamestate.playerlist = msgdata['playerlist']
+    }
+
     console.log(msgdata)
     gamestate.playerdata = JSON.parse(msgdata);
     // emit an event so that we know the message came in
