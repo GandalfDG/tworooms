b26b1e0c7a807115099357e159a8a63f1d4d8e0a
Jack Case <jackacase@gmail.com>
2022-08-11 10:36:37 -0400

    completed basic cards in the cardmap

--- frontend/src/cardmap.js
+++ frontend/src/cardmap.js
@@ -10,5 +10,23 @@ export default cardmap = {
         image: "bomber",
         description: "lorem ipsum",
         summary: "lipsum"
+    },
+    blue_team: {
+        color: "blue",
+        image: "blue_team",
+        description: "blue team",
+        summary: "blue"
+    },
+    red_team: {
+        color: "red",
+        image: "red_team",
+        description: "red team",
+        summary: "red"
+    },
+    gambler: {
+        color: "grey",
+        image: "gambler",
+        description: "grey gambler",
+        summary: "very cool guy"
     }
 }
