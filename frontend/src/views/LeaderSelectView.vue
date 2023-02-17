12ac4dedab462d4e02ee475c181400968e92d06a
Jack Case <jackacase@gmail.com>
2023-02-17 22:03:54 +0000

    fixup the leader select buttons to use fullwidth

--- frontend/src/views/LeaderSelectView.vue
+++ frontend/src/views/LeaderSelectView.vue
@@ -6,7 +6,14 @@
 </script>
 
 <template>
-    <ColumnList :list="gamestate.roommates" :columns="2"><template v-slot="value">
-        <input class="button" :value="value.value"/>
-    </template></ColumnList>
+    <div class="is-flex is-flex-direction-column" style="height:100vh">
+        <div class="block mt-5">
+            <h1 class="title is-3">Select your room's leader</h1>
+        </div>
+        <ColumnList :list="gamestate.roommates" :columns="2">
+            <template v-slot="playername">
+                <button class="button is-fullwidth">{{ playername.value }}</button>
+            </template>
+        </ColumnList>   
+    </div>
 </template>
