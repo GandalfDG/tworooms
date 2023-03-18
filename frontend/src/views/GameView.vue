5f2e7c9add554e335058381d84fc0812ef1a2345
Jack Case <jackacase@gmail.com>
2023-03-18 21:54:31 +0000

    notification to top, leader name in header when selected

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -34,16 +34,8 @@ function roundEnded() {
 </script>
 
 <template>
-  <h2>The room leader must select {{ hostages }} hostage{{ hostages > 1 ? 's' : '' }} in</h2>
-  <Timer :duration=roundDuration :paused=false :start-timestamp=gamestate.start_timestamp @time-elapsed="roundEnded()" />
-  <Card v-model:visibility="visibility" v-model:cardname="gamestate.card.name" />
-  <div class="buttoncontainer">
-    <button @click="visibility = 'color'">Color Reveal</button>
-    <button @click="visibility = 'full'">Full Reveal</button>
-    <button @click="visibility = 'none'">Hide Card</button>
-  </div>
-
-  <div class="notification is-primary" @click="leaderSelectVisibility = true" v-if="!gamestate.roomleader">
+  <!-- Leader select notification -->
+  <div class="notification is-primary mt-2" @click="leaderSelectVisibility = true" v-if="!gamestate.roomleader">
     <div class="level is-mobile">
       <div class="level-item is-flex-shrink-1">Select a leader for the room! The first person selected will become the leader, so act fast!</div>
       <div class="level-item">
@@ -54,6 +46,16 @@ function roundEnded() {
         </button>
       </div>
     </div>
+  </div>
+  <h1 v-if="gamestate.roomleader">
+    <span class="has-text-primary has-text-weight-bold">{{gamestate.roomleader}}</span> must select {{ hostages }} hostage{{ hostages > 1 ? 's' : '' }} in:
+  </h1>
+  <Timer :duration=roundDuration :paused=false :start-timestamp=gamestate.start_timestamp @time-elapsed="roundEnded()" />
+  <Card v-model:visibility="visibility" v-model:cardname="gamestate.card.name" />
+  <div class="buttoncontainer">
+    <button @click="visibility = 'color'">Color Reveal</button>
+    <button @click="visibility = 'full'">Full Reveal</button>
+    <button @click="visibility = 'none'">Hide Card</button>
   </div>
 
   <Modal :active="leaderSelectVisibility" @deactivate="leaderSelectVisibility = false">
