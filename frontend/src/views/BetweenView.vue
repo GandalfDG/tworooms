d9ea93b3c77c170427b36bb2a2bebdc7f8343a86
Jack Case <jackacase@gmail.com>
2023-03-26 20:23:42 +0000

    display list of eligible hostages

--- frontend/src/views/BetweenView.vue
+++ frontend/src/views/BetweenView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { getRoundHostages } from '@/gamelogic';
 import { useGameState } from '@/stores/gamestate';
+import ColumnList from '@/components/ColumnList.vue'
 
 const gamestate = useGameState();
 
@@ -18,6 +19,15 @@ function nextRound() {
         <h1 class="title is-4">Round Ended, time to transfer {{hostages}} hostage{{hostages>1?'s':''}}
         </h1>
     </div>
+    
+    <div v-if="gamestate.isleader">
+        <ColumnList :list="gamestate.roommates" :columns="2">
+            <template v-slot="playername">
+                <button class="button is-fullwidth mb-2" :class="{ 'is-primary': playername.value === selectedPlayer }" :value="playername.value" @click="selectName(playername.value)">{{ playername.value }}</button>
+            </template>
+        </ColumnList>   
+    </div>
+
     <div class="field" v-if="gamestate.ishost">
         <p class="control">
             <button class="button is-link is-large" @click="nextRound()">Next Round</button>
